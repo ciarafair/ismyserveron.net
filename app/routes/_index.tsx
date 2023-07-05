e0cd998adc2fcf9b26bccea1d81e5431328f57fb
ciarafair <138089831+ciarafair@users.noreply.github.com>
2023-07-05 23:24:08 +1000

    God help us. Maybe d67b352?

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,14 +3,14 @@ import type { V2_MetaFunction } from "@remix-run/cloudflare";
 export const meta: V2_MetaFunction = () => {
 	return [
 		{ title: "Is My Server On?" },
-		{ name: "description", content: "Server Check" },
+		{ name: "description", content: "Is My Server Running?" },
 	];
 };
 
 export default function Index() {
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-			<h1>Yes. Yes it is.</h1>
+			<h1>Yes, my server is in fact running.</h1>
 		</div>
 	);
 }
