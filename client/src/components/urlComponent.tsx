464e3b44d6b938aa99c2d52aed6f213ebc5951ad
ciarafair <138089831+ciarafair@users.noreply.github.com>
2023-08-28 21:00:55 +1000

    Updated nav bar for ssl backend

--- client/src/components/urlComponent.tsx
+++ client/src/components/urlComponent.tsx
@@ -14,7 +14,7 @@ const GetUrl: React.FC = () => {
 		console.log('[Ping] Sending PUT to /url');
 		axios({
 			method: 'get',
-			url: 'https://nginx.ismyserveron.net/url',
+			url: 'https://backend2.ismyserveron.net/url',
 			headers: {
 				'Access-Control-Allow-Origin': '*',
 				'Content-Type': 'application/json'
