76b1e9ce5d74adabb9948ef223afdc9c13b67d82
ciarafair <138089831+ciarafair@users.noreply.github.com>
2023-08-29 23:40:30 +1000

    Reverted changes

--- server/app/lib/api/testRes.js
+++ server/app/lib/api/testRes.js
@@ -1,13 +1,23 @@
 import winstonLogger from "../winstonFunction.js";
 
 const TWO_HUNDRED = 200;
+const ZERO = 0;
+const EIGHT = 8;
 const ONE = 1;
 
 const updateTime = () => {
-	const currentTime = new Date().
-		getTime().
-		valueOf().
-		toString();
+	const milliseconds = new Date().getMilliseconds().
+		toString().
+		slice(ZERO, EIGHT);
+	const seconds = new Date().getSeconds().
+		toString().
+		slice(ZERO, EIGHT);
+	const minutes = new Date().getMinutes().
+		toString().
+		slice(ZERO, EIGHT);
+	const hours = new Date().getHours().
+		slice(ZERO, EIGHT);
+	const currentTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
 	return currentTime;
 };
 
