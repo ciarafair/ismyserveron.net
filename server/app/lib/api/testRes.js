38dee9128d2713ce6e6e6a64779eaa319b171e47
ciarafair <138089831+ciarafair@users.noreply.github.com>
2023-08-29 21:43:26 +1000

    Fixed bug in testRes.js

--- server/app/lib/api/testRes.js
+++ server/app/lib/api/testRes.js
@@ -1,11 +1,11 @@
-import { time } from "console";
 import winstonLogger from "../winstonFunction.js";
 
 const TWO_HUNDRED = 200;
 
 const testGet = (req, res) => {
-	res.status(TWO_HUNDRED).send(time);
+	const currentTime = new Date().toString();
+	res.status(TWO_HUNDRED).send(currentTime);
 	winstonLogger.info("Got a GET request at /test");
 };
 
-export { testGet };
+export default testGet;
