f25171eae4d9be08ae3ed73f8dc4fbbd5fa39434
ciarafair <138089831+ciarafair@users.noreply.github.com>
2023-08-29 13:18:36 +1000

    Added ./favicon to an express static path

--- server/app/index.js
+++ server/app/index.js
@@ -24,7 +24,7 @@ ExpressApp.use(cors());
 ExpressApp.set("port", process.env.PORT);
 ExpressApp.use("/", ExpressRouter);
 ExpressApp.use(express.json());
-ExpressApp.use(express.static("../public"));
+ExpressApp.use("/static", express.static("../public"));
 
 ExpressApp.get("/api", (req, res) => {
 	urlPost(req, res);
