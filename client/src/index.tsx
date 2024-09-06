a61b0f1c396ad9211aea68d9d57e85612c04fdd0
Ciar <138089831+ciarafair@users.noreply.github.com>
2024-09-06 15:14:03 +1000

    Moved CSS to public

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
-import './css/index.css'
 
 // @ts-expect-error annoying null error
 const rootElement: HTMLElement = document.getElementById('root')
