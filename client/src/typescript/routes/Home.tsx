df7ffd0a377e0b6c13de766d97068d19527d6328
Ciar <138089831+ciarafair@users.noreply.github.com>
2024-09-06 15:04:45 +1000

    commented logs, added 8ball quotes

--- client/src/typescript/routes/Home.tsx
+++ client/src/typescript/routes/Home.tsx
@@ -2,16 +2,24 @@
 import * as React from 'react'
 import DeveloperCommand from '../components/developerCommand'
 
-const possibleStrings: Array<string> = ["Why are you asking me? I have no clue.", "It might be, you should probably go check that.", "I eated it.", "Have you tried turning it on and off again?", "start maybe.exe"]
+const possibleStrings: Array<string> = [
+	"Why are you asking me? I have no clue.",
+	"It might be, you should probably go check that.",
+	"I eated it.", "Have you tried turning it on and off again?",
+	"start maybe.exe",
+	"Reply hazy, try again",
+	"Better not tell you now"
+]
+
 const chosenString: string = possibleStrings[Math.floor(Math.random()*possibleStrings.length)]
 
 function setElementAttribute() {
 	// @ts-expect-error annoying null error
 	const root: Element = document.body.querySelector("#root")
-	console.log("Found root element: ", root)
+	//console.log("Found root element: ", root)
 	// @ts-expect-error annoying null error
 	const selection: Element = root.querySelector(".command")
-	console.log("Found selection: ", selection)
+	//console.log("Found selection: ", selection)
 	if (selection != null) {
 		document.documentElement.style.setProperty('--animationSteps', String(chosenString.length))
 		document.documentElement.style.setProperty('--animationTime', String(25 * chosenString.length))
