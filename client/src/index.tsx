1dce2c3500fb55afbf961f3a5e51b851a0e2f750
Ciar <138089831+ciarafair@users.noreply.github.com>
2024-09-14 15:35:15 +1000

    minor fixes to build

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
