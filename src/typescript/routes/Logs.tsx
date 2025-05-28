fe290011cb465605b2d3c5517caff4d856de36dc
ciarafair <138089831+ciarafair@users.noreply.github.com>
2025-05-28 23:18:14 +1000

    Added scrollbar feature for hiding it during non-neccesary pages

--- src/typescript/routes/Logs.tsx
+++ src/typescript/routes/Logs.tsx
@@ -7,12 +7,13 @@ import { Sidebar } from '../components/Focusables.tsx'
 import { emitter } from '../scripts/eventBus.ts'
 
 function Logs(this: any): React.ReactElement {
-	const optionOneContent: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis in mauris ac ultrices. In interdum congue finibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod mi orci, sit amet rhoncus felis ultricies vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed eleifend dui, vitae facilisis ante. Aliquam lobortis eget eros eget placerat. Etiam maximus faucibus erat, vel efficitur neque suscipit sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque dictum orci eu laoreet eleifend. Suspendisse potenti. Morbi imperdiet velit a massa pulvinar tristique. Suspendisse auctor elit sapien, ultrices ornare tellus cursus sed. Sed vitae accumsan elit, quis egestas velit.\n Donec in lectus laoreet, elementum libero id, auctor nisi. Proin in augue nec nisi ultrices dictum et dapibus libero. Praesent congue tempus congue. Maecenas scelerisque pellentesque felis, non fringilla augue commodo ac. Duis cursus nisl rhoncus venenatis ultrices. Nulla leo ex, aliquet nec finibus at, eleifend ac lorem. Ut interdum porttitor sem cursus pulvinar. Etiam elit dui, scelerisque eu blandit a, volutpat non quam. Aenean egestas ipsum eu urna placerat facilisis. Nunc fringilla nisl a ante auctor volutpat. Morbi ac vehicula elit. Cras venenatis eros elit, non ultricies nisl accumsan et. Phasellus ultricies, sem vel congue iaculis, enim dui posuere nisl, eget convallis neque sapien eu risus.\n Phasellus faucibus pretium vulputate. Phasellus rhoncus molestie lectus id faucibus. Donec tortor elit, accumsan sed sollicitudin non, tincidunt eu est. In dapibus diam nec dui tincidunt malesuada. Phasellus pellentesque suscipit turpis non consequat. Proin sed ornare magna. Fusce quis lectus dui. Aliquam erat volutpat. Aliquam dignissim arcu posuere auctor imperdiet. Ut iaculis sodales sodales. Mauris blandit tristique dui, non consequat enim iaculis et. Aliquam lobortis gravida nulla, vel porttitor ante accumsan eu. Phasellus pharetra ante efficitur lectus laoreet, quis tempor nulla auctor. Vivamus sem leo, porttitor quis magna eu, rutrum congue tellus. Integer lacinia finibus ante at venenatis.\n Sed faucibus porta nisl eget volutpat. Sed eget convallis dui, eget suscipit elit. Fusce condimentum tempus ante, finibus commodo orci consequat vitae. Nulla interdum non dui consequat vestibulum. Vestibulum eget lacinia nibh. Aenean ultrices vehicula feugiat. Duis congue lacus et ultricies porta. Ut feugiat condimentum mollis. Suspendisse in lorem nisi. In vulputate eu elit sit amet tincidunt. Maecenas suscipit, libero in sollicitudin fermentum, sapien mauris volutpat quam, eget pellentesque est neque quis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Suspendisse sagittis leo et iaculis vestibulum. Sed ut faucibus lorem. Donec eu ante mattis, pulvinar metus id, venenatis ante. Maecenas interdum posuere enim, id faucibus dolor condimentum sit amet. Quisque sapien nisi, varius et facilisis at, consectetur id mi. Morbi tempus vel lorem nec accumsan. Vivamus nec imperdiet nulla. Nullam non viverra nisi, sit amet placerat ligula. Fusce lobortis lectus a magna maximus, non lobortis risus ullamcorper. Quisque rutrum purus et aliquet vestibulum. Nulla volutpat malesuada augue, eu molestie libero faucibus et. Mauris imperdiet aliquam purus sit amet fringilla. Vivamus ac erat commodo, molestie dolor quis, tincidunt tellus. Etiam facilisis orci purus, eu maximus velit accumsan commodo.\n Aenean tincidunt fermentum efficitur. Sed varius ut tellus eget ornare. Phasellus non diam in felis tempor condimentum eu non lectus. Pellentesque a justo pellentesque, tincidunt ligula quis, aliquet enim. Suspendisse potenti. Curabitur rhoncus volutpat ex, dignissim feugiat diam molestie et. Cras eget lectus in justo volutpat hendrerit. Quisque quis rutrum magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eget convallis massa. Praesent sit amet nulla feugiat, rutrum tellus non, egestas elit. Sed eget bibendum ligula, et pretium nisl. Duis tincidunt finibus elit, non bibendum sapien viverra in. Vivamus eu velit urna. Curabitur.\n This text should not be visible.\n`
+	const optionOneContent: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis in mauris ac ultrices. In interdum congue finibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod mi orci, sit amet rhoncus felis ultricies vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed eleifend dui, vitae facilisis ante. Aliquam lobortis eget eros eget placerat. Etiam maximus faucibus erat, vel efficitur neque suscipit sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque dictum orci eu laoreet eleifend. Suspendisse potenti. Morbi imperdiet velit a massa pulvinar tristique. Suspendisse auctor elit sapien, ultrices ornare tellus cursus sed. Sed vitae accumsan elit, quis egestas velit.\n Donec in lectus laoreet, elementum libero id, auctor nisi. Proin in augue nec nisi ultrices dictum et dapibus libero. Praesent congue tempus congue. Maecenas scelerisque pellentesque felis, non fringilla augue commodo ac. Duis cursus nisl rhoncus venenatis ultrices. Nulla leo ex, aliquet nec finibus at, eleifend ac lorem. Ut interdum porttitor sem cursus pulvinar. Etiam elit dui, scelerisque eu blandit a, volutpat non quam. Aenean egestas ipsum eu urna placerat facilisis. Nunc fringilla nisl a ante auctor volutpat. Morbi ac vehicula elit. Cras venenatis eros elit, non ultricies nisl accumsan et. Phasellus ultricies, sem vel congue iaculis, enim dui posuere nisl, eget convallis neque sapien eu risus.\n Phasellus faucibus pretium vulputate. Phasellus rhoncus molestie lectus id faucibus. Donec tortor elit, accumsan sed sollicitudin non, tincidunt eu est. In dapibus diam nec dui tincidunt malesuada. Phasellus pellentesque suscipit turpis non consequat. Proin sed ornare magna. Fusce quis lectus dui. Aliquam erat volutpat. Aliquam dignissim arcu posuere auctor imperdiet. Ut iaculis sodales sodales. Mauris blandit tristique dui, non consequat enim iaculis et. Aliquam lobortis gravida nulla, vel porttitor ante accumsan eu. Phasellus pharetra ante efficitur lectus laoreet, quis tempor nulla auctor. Vivamus sem leo, porttitor quis magna eu, rutrum congue tellus. Integer lacinia finibus ante at venenatis.\n Sed faucibus porta nisl eget volutpat. Sed eget convallis dui, eget suscipit elit. Fusce condimentum tempus ante, finibus commodo orci consequat vitae. Nulla interdum non dui consequat vestibulum. Vestibulum eget lacinia nibh. Aenean ultrices vehicula feugiat. Duis congue lacus et ultricies porta. Ut feugiat condimentum mollis. Suspendisse in lorem nisi. In vulputate eu elit sit amet tincidunt. Maecenas suscipit, libero in sollicitudin fermentum, sapien mauris volutpat quam, eget pellentesque est neque quis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Suspendisse sagittis leo et iaculis vestibulum. Sed ut faucibus lorem. Donec eu ante mattis, pulvinar metus id, venenatis ante. Maecenas interdum posuere enim, id faucibus dolor condimentum sit amet. Quisque sapien nisi, varius et facilisis at, consectetur id mi. Morbi tempus vel lorem nec accumsan. Vivamus nec imperdiet nulla. Nullam non viverra nisi, sit amet placerat ligula. Fusce lobortis lectus a magna maximus, non lobortis risus ullamcorper. Quisque rutrum purus et aliquet vestibulum. Nulla volutpat malesuada augue, eu molestie libero faucibus et. Mauris imperdiet aliquam purus sit amet fringilla. Vivamus ac erat commodo, molestie dolor quis, tincidunt tellus. Etiam facilisis orci purus, eu maximus velit accumsan commodo.\n Aenean tincidunt fermentum efficitur. Sed varius ut tellus eget ornare. Phasellus non diam in felis tempor condimentum eu non lectus. Pellentesque a justo pellentesque, tincidunt ligula quis, aliquet enim. Suspendisse potenti. Curabitur rhoncus volutpat ex, dignissim feugiat diam molestie et. Cras eget lectus in justo volutpat hendrerit. Quisque quis rutrum magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eget convallis massa. Praesent sit amet nulla feugiat, rutrum tellus non, egestas elit. Sed eget bibendum ligula, et pretium nisl. Duis tincidunt finibus elit, non bibendum sapien viverra in. Vivamus eu velit urna. Curabitur.\n This text should not be visible without scrolling.\n`
 	const optionTwoContent: string = `test`
 
 	const [content, setContent] = useState(optionOneContent);
 	const handleClick = (content: string) => {
 		setContent(content);
+		handleHide()
 	};
 
 	function LogsButtonOne(): React.ReactElement {
@@ -35,13 +36,27 @@ function Logs(this: any): React.ReactElement {
 		)
 	}
 
-
 	let scrollContainer: HTMLElement = document.querySelector('.scroll') as HTMLElement;
 
-	const handleScroll = (direction: string) => {
-		scrollContainer = document.querySelector('.scroll') as HTMLElement;
-		if (!scrollContainer) return;
+	const isScrollable = function (element: Element, axis: 'y' | 'x' = 'y'): boolean {
 
+		const elementParent: HTMLElement = scrollContainer.parentElement;
+
+		if (elementParent == null) {
+			console.log('Error!')
+			return false
+		}
+
+		if (axis === 'y') {
+			//console.log(element.scrollHeight > elementParent.clientHeight)
+			return element.scrollHeight > elementParent.clientHeight;
+		} else {
+			//console.log(element.scrollWidth > elementParent.clientWidth)
+			return element.scrollWidth > elementParent.clientWidth;
+		}
+	};
+
+	const handleScroll = (direction: string) => {
 		const lineHeight = parseFloat(getComputedStyle(scrollContainer).lineHeight) * 3;
 
 		if (direction === 'down') {
@@ -52,11 +67,30 @@ function Logs(this: any): React.ReactElement {
 			scrollContainer.scrollBy({ top: -lineHeight, behavior: 'smooth' });
 		}
 	}
-	if (scrollContainer != null) {
-		if (scrollContainer.scrollTop === (scrollContainer.scrollHeight - scrollContainer.offsetHeight)) {
-			console.log('test')
-		} else {
-			console.log('test2')
+
+	const handleHide = () => {
+		if (!scrollContainer) return;
+
+		scrollContainer = document.querySelector('.scroll') as HTMLElement;
+		const indicators = document.querySelectorAll('.indicator');
+
+		if (isScrollable(scrollContainer, 'y')) {
+
+			indicators.forEach(function (element) {
+				element.classList.remove("inactive")
+				console.log(element)
+			})
+			console.log(`Container is not scrollable: ${isScrollable(scrollContainer, 'y')}.`)
+		}
+		else {
+			if (indicators != null) {
+				indicators.forEach(function (element) {
+					element.classList.add("inactive")
+					console.log(element)
+				})
+
+				console.log(`Container is scrollable: ${isScrollable(scrollContainer, 'y')}.`)
+			}
 		}
 	}
 
@@ -68,11 +102,23 @@ function Logs(this: any): React.ReactElement {
 	}
 
 	useEffect(() => {
+		const indicators = document.querySelectorAll('.indicator');
+		scrollContainer = document.querySelector('.scroll') as HTMLElement;
+
 		document.addEventListener('load', this, true); {
 			emitter.emit('routeLoaded', 'Logs.tsx')
 			applyBorders()
+			if (!scrollContainer) return;
+
+			if (isScrollable(scrollContainer, 'y')) {
+				indicators.forEach(el => el.classList.remove('inactive'));
+				console.log('Scroll enabled');
+			} else {
+				indicators.forEach(el => el.classList.add('inactive'));
+				console.log('Scroll disabled');
+			}
 		}
-	}, []);
+	}, [content]);
 
 	return (
 		<>
